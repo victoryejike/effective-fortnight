5fd1d353cf8d97b55f243062dd72983e519c80c9
victoryejike <victoryejike@gmail.com>
2023-11-02 00:16:04 +0100

    feat: set up the cavas for displaying the shirt

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,5 +1,25 @@
-const Canvas = () => {
-  return <div>Canvas</div>;
+/* eslint-disable react/no-unknown-property */
+import { Canvas } from "@react-three/fiber";
+import { Environment, Center } from "@react-three/drei";
+
+import Shirt from "./Shirt";
+import CameraRig from "./CameraRig";
+import Backdrop from "./Backdrop";
+
+const CanvasModel = () => {
+  return (
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <Environment preset="city" />
+
+      {/* <CameraRig> */}
+      {/* <Backdrop /> */}
+      <Center>
+        <Shirt />
+      </Center>
+      {/* </CameraRig> */}
+    </Canvas>
+  );
 };
 
-export default Canvas;
+export default CanvasModel;
