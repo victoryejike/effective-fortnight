fdd2a0d11c94a9747ba8465a4216ad64fbde1402
victoryejike <victoryejike@gmail.com>
2023-11-01 03:20:39 +0100

    feat: created the customizer page alongside other tab components

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -23,7 +23,7 @@ const CustomButton = ({ type, title, customStyles, handleClick }) => {
   };
   return (
     <button
-      className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
+      className={`px-2 py-1.5 flex-1 mt-4 rounded-md ${customStyles}`}
       style={generateStyle(type)}
       onClick={handleClick}
     >
